6f97e06447d860922ac7c4de6f145355e9b5d4f9
bc37 <bc37@st-andrews.ac.uk>
2017-05-03 00:52:08 +0100

    Fix code to now display textures

--- shaders/vertex_attributes.cpp
+++ shaders/vertex_attributes.cpp
@@ -58,25 +58,6 @@ std::unique_ptr<std::string> loadShaderString(std::string fn) {
     return content;
 }
 
-void loadImage() {
-    int width, height;
-    unsigned char* image = SOIL_load_image("container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
-    if (!image) {
-        std::cout << "Failed to load texture: " << sizeof(image) << std::endl;
-    }
-
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    SOIL_free_image_data(image);
-    glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-
-
-
 
 int main(int argc, char* args[]) {
     sdlSetup();
@@ -105,6 +86,7 @@ int main(int argc, char* args[]) {
         0, 1, 3, // First Triangle
         1, 2, 3  // Second Triangle
     };
+
     GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -132,7 +114,7 @@ int main(int argc, char* args[]) {
 
 
     // Load and create a texture
-    GLuint texture1;
+    GLuint texture1, texture2;
     // ====================
     // Texture 1
     // ====================
@@ -146,87 +128,33 @@ int main(int argc, char* args[]) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // Load, create texture and generate mipmaps
     int width, height;
-    unsigned char* image = SOIL_load_image("t.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+    unsigned char* image = SOIL_load_image("container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
     SOIL_free_image_data(image);
     glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
+    // ===================
+    // Texture 2
+    // ===================
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // Set our texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Set texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Load, create texture and generate mipmaps
+    image = SOIL_load_image("awesomeface.png", &width, &height, 0, SOIL_LOAD_RGB);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
-   //  Shader ourShader("vertex_shader.txt", "fragment_shader.txt");
-   //
-   //  // Set up vertex data (and buffer(s)) and attribute pointers
-   //  GLfloat vertices[] = {
-   //      // Positions          // Colors           // Texture Coords
-   //       0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top Right
-   //       0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom Right
-   //      -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom Left
-   //      -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top Left
-   //  };
-   //
-   //  GLuint indices[] = {  // Note that we start from 0!
-   //      0, 1, 3, // First Triangle
-   //      1, 2, 3  // Second Triangle
-   //  };
-   //
-   //  // Load and create a texture
-   //  GLuint texture;
-   //  glGenTextures(1, &texture);
-   //  glBindTexture(GL_TEXTURE_2D, texture); // All upcoming GL_TEXTURE_2D operations now have effect on this texture object
-   //  // Set the texture wrapping parameters
-   //  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
-   //  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-   //  // Set texture filtering parameters
-   //  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-   //  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-   //  // Load image, create texture and generate mipmaps
-   //  int width, height;
-   //  unsigned char* image = SOIL_load_image("t.jpg", &width, &height, 0, SOIL_LOAD_RGB);
-   //  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-   //  glGenerateMipmap(GL_TEXTURE_2D);
-   //  SOIL_free_image_data(image);
-   //  glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
-   //
-   //
-   //  GLuint VBO, VAO, EBO;
-   //  glGenVertexArrays(1, &VAO);
-   //  glGenBuffers(1, &VBO);
-   //  // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-   //  glBindVertexArray(VAO);
-   //
-   // glGenBuffers(1, &EBO);
-   // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-   // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-   //
-   //  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-   //  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-   //
-   //  // Position attribute
-   //  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
-   //  glEnableVertexAttribArray(0);
-   //  // Color attribute
-   //  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
-   //  glEnableVertexAttribArray(1);
-   //  // Texture attribute
-   //  glVertexAttribPointer(2, 2, GL_FLOAT,GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-   //  glEnableVertexAttribArray(2);
-   //
-   //  glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
-   //  glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+
 
     SDL_Event windowEvent;
     while (true) {
-        // Render
-        // Clear the colorbuffer
-//        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-//        glClear(GL_COLOR_BUFFER_BIT);
-        // Draw our first triangle
-//        ourShader.Use();
-        //GLfloat timeValue = SDL_GetTicks()/1000.0f;
-        //GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
-        //GLint vertexColorLocation = glGetUniformLocation(ourShader.program, "vertexColor");
-        //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
-
         // Render
         // Clear the colorbuffer
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -238,20 +166,16 @@ int main(int argc, char* args[]) {
         // Bind Textures using texture units
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
-        glUniform1i(glGetUniformLocation(ourShader.program, "ourTexture"), 0);
+        glUniform1i(glGetUniformLocation(ourShader.program, "ourTexture1"), 0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glUniform1i(glGetUniformLocation(ourShader.program, "ourTexture2"), 1);
 
         // Draw container
         glBindVertexArray(VAO);
+        // //glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
-        // glActiveTexture(GL_TEXTURE0);
-        // glBindTexture(GL_TEXTURE_2D, texture1);
-        // glUniform1i(glGetUniformLocation(ourShader.program, "ourTexture"), 0);
-        //
-        // glBindVertexArray(VAO);
-        // //glDrawArrays(GL_TRIANGLES, 0, 3);
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0);
 
         if (SDL_PollEvent(&windowEvent)) {
             if (windowEvent.type == SDL_QUIT) break;
